b4ddfa500da43a4844c477c57a3f202635c3145b
SzDabrowski <szdabrowsky@gmail.com>
2023-08-07 21:03:38 +0200

    chores: code review fixes

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -2,12 +2,12 @@ import styles from "./AppHeader.module.css";
 import logotype from "../assets/header_logo.svg";
 import button from "../assets/header_button.svg";
 
-export const AppHeader = () => (
+export const AppHeader = (cardsNumber=0) => (
   <header className={styles.header}>
     <div className="cards-counter">
         <img src={logotype} alt={"logo"} />
         <h1 className="header">Cards:</h1>
-        <span className="cardsNumber">0</span>
+        <span className="cardsNumber">{cardsNumber}</span>
     </div >
       <button>
       <img src={button} alt={"button"} />
