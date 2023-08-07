7194afdcf42335727edd47868c887264e3da156c
SzDabrowski <szdabrowsky@gmail.com>
2023-08-07 20:57:50 +0200

    feat/header-component

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,5 +1,17 @@
 import styles from "./AppHeader.module.css";
+import logotype from "../assets/header_logo.svg";
+import button from "../assets/header_button.svg";
 
 export const AppHeader = () => (
-  <header className={styles.header}>Fischkapp header</header>
+  <header className={styles.header}>
+    <div className="cards-counter">
+        <img src={logotype} alt={"logo"} />
+        <h1 className="header">Cards:</h1>
+        <span className="cardsNumber">0</span>
+    </div >
+      <button>
+      <img src={button} alt={"button"} />
+      </button>
+
+  </header>
 );
