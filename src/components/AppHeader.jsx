a36a27f71f8d00e236a8acbb73829d2fc5d87902
SzDabrowski <szdabrowsky@gmail.com>
2023-08-08 17:12:28 +0200

    chores: code review fixes 4

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,19 +1,25 @@
+import React, {useState} from 'react';
 import styles from "./AppHeader.module.css";
 import logotype from "../assets/header_logo.svg";
 import button from "../assets/header_button.svg";
 
-export function AppHeader({}){
 
- const cardsNumber = 0;
+export function AppHeader({ cardsNumber }){
+
+  const [currentCardsNumber, setCurrentCardsNumber] = useState(cardsNumber || 0);
+
+  const addNumber = () => {
+    setCurrentCardsNumber(currentCardsNumber + 1);
+  };
     
 
   return (<header className={styles.header}>
     <div>
         <img src={logotype} alt={"logo"} />
         <h1 className="header">Cards:</h1>
-        <span className="cardsNumber">{cardsNumber}</span>
+        <span className="cardsNumber">{currentCardsNumber}</span>
     </div >
-      <button>
+     <button className="addButton" onClick={addNumber}>
       <img src={button} alt={"add card button"} />
       </button>
 
